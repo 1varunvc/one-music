2000d6318929c125f356b34f96ded605a71f2411
1varunvc <71169556+1varunvc@users.noreply.github.com>
2021-05-17 20:13:02 +0530

    Add files via upload

---
+++ test/app.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const https = require("https");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.set('view engine', 'ejs');
+
+var videoId0 = "";
+var result = "";
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req, res) {
+  const query = req.body.queryValue;
+  const apiKey = "AIzaSyC5asO20CAohIW_wEkgJB0XHVH4bOZSN5U"
+  const url = "https://www.googleapis.com/youtube/v3/search?key=" + apiKey + "&part=snippet&q=" + query + "&maxResults=1&order=relevance&type=video";
+
+  https.get(url, (response) => {
+    const chunks = []
+    response.on('data', (d) => {
+      chunks.push(d)
+    })
+
+    response.on('end', () => {
+      var result = JSON.parse((Buffer.concat(chunks).toString()))
+      console.log(result)
+
+
+      var videoId0 = result.items[0].id.videoId;
+      console.log("Fetched value: " + videoId0);
+
+      res.render("results", {
+        iFrameVideoId0: videoId0
+      });
+      console.log("Rendered value to be sent: " + videoId0);
+
+    });
+  });
+});
+
+app.listen(3000, function() {
+  console.log("Server is running on port 3000.")
+});
